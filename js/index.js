9b9f1676cc805b814a94d5cb32b1e1bba01783a8
s3822042 <s3822042@rmit.edu.vn>
2021-04-26 14:53:44 +0700

    fix handling event for login form

--- js/index.js
+++ js/index.js
@@ -53,7 +53,6 @@ function validateLogin() {
   var emailAddress = "email";
   var password = "password";
   if (email == emailAddress && pass == password) {
-    email1 = email;
     localStorage.setItem("validatedEmailAddress", emailAddress);
     return true;
   } else {
@@ -61,58 +60,12 @@ function validateLogin() {
     return false;
   }
 }
-let dataLocalStorage = document.getElementById("email");
-dataLocalStorage.value = localStorage.getItem("validatedEmailAddress");
-dataLocalStorage.disabled = true;
+function displayUserInput() {
+  let dataLocalStorage = document.getElementById("email");
+  dataLocalStorage.value = localStorage.getItem("validatedEmailAddress");
+  dataLocalStorage.disabled = true;
 
-// Registration Data
-var first_name = document.forms["regForm"]["first-name"];
-var last_name = document.forms["regForm"]["last-name"];
-var email = document.forms["regForm"]["email"];
-var password = document.forms["regForm"]["password"];
-var password_confirmation = document.forms["regForm"]["confirm-password"];
-
-username.addEventListener("blur", nameVerify, true);
-email.addEventListener("blur", emailVerify, true);
-password.addEventListener("blur", passwordVerify, true);
-
-const submitBtn = document.getElementById("submit-btn");
-
-const validate = (e) => {
-  e.preventDefault();
-  //validate first name
-  const firstName = document.getElementById("firstName");
-  if (firstName.value === "") {
-    alert("Please enter your first name.");
-    firstName.focus();
-    return false;
-  }
-  // validate last name
-  const lastName = document.getElementById("lastName");
-  if (lastName.value === "") {
-    alert("Please enter your last name.");
-    lastName.focus();
-    return false;
-  }
-
-  // validate email
-  const emailAddress = document.getElementById("email-address");
-  if (emailAddress.value === "") {
-    alert("Please enter your email address.");
-    emailAddress.focus();
-    return false;
-  }
-  if (!emailIsValid(emailAddress.value)) {
-    alert("Please enter a valid email address.");
-    emailAddress.focus();
-    return false;
-  }
-  return true;
-};
-const emailIsValid = (email) => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-};
-submitBtn.addEventListener("click", validate);
+}
 
 var previouslyEnteredEmail = getData("validatedEmailAddress");
 if (previouslyEnteredEmail !== null) {
