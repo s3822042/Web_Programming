5866791abd3f0439ae6723979570bdeb2a8f458c
s3822042 <s3822042@rmit.edu.vn>
2021-05-08 11:33:43 +0700

    add email validation for login

--- js/index.js
+++ js/index.js
@@ -137,12 +137,16 @@ function validateLogin() {
 
     let email = document.loginForm.email.value;
     let pass = document.loginForm.pwd.value;
-
+    
+    let emailRegex = /(?:[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
     let password = "password";
-    if (pass == password) {
+    if (emailRegex.test(email)){
+        if (pass == password) {
         localStorage.setItem("validatedEmailAddress", email);
         return true;
-    } else {
+    }
+    }
+     else {
         alert("Login was unsuccessful, please check your email and/or password");
         return false;
     }
