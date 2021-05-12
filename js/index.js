d6558b60494d008949c57c1a8544230714bf52d2
Andrew Le <s3820098@rmit.edu.vn>
2021-05-12 18:15:25 +0700

    feat: add php + adjust files content
    add php + adjust files content TO verify ADMIN USER

--- js/index.js
+++ js/index.js
@@ -141,7 +141,7 @@ function validateLogin() {
   let emailRegex = /(?:[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
   let password = "password";
   if (emailRegex.test(email)) {
-    if (pass == password) {
+    if (pass == password || pass == "$2y$10VJZeRekNVK8dodKsQubVcOa5PupDhGH4JYyr6lS4CxdzzTJ.7/cK6") {
       localStorage.setItem("validatedEmailAddress", email);
       return true;
     }
