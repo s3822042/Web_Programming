eddb316af1f705e4edc57b19e15e7648944d833d
s3822042 <s3822042@rmit.edu.vn>
2021-05-19 15:56:54 +0700

    initial design for store homepage

--- templates/store/Store_homepage.php
+++ templates/store/Store_homepage.php
@@ -67,7 +67,97 @@ fclose($product_file);
 </head>
 
 <body>
-    
+    <!-- Navigation bar -->
+    <header>
+        <!-- Logo -->
+        <div class="brand"> <a href="../../index.php"><img src="https://i.imgur.com/mE6aWmB.png" alt="logo" class="logo-img" /> </a> </div>
+        <!-- Right menu -->
+        <nav class="menu">
+            <input type="checkbox" id="menuToggle" />
+            <label for="menuToggle" class="menu-icon"><i class="fa fa-bars"></i></label>
+            <ul>
+                <a href="../about.php">
+                    <li>About us</li>
+                </a> <a href="../fees.html">
+                    <li>Fees</li>
+                </a> <a href="../account/account.php">
+                    <li>Account</li>
+                </a> <a href="../browse-menu.html">
+                    <li>Browse</li>
+                </a> <a href="../faq.html">
+                    <li>FAQs</li>
+                </a> <a href="../contact.html">
+                    <li>Contact</li>
+                </a> <a href="../login-form.php">
+                    <li>Sign in</li>
+                </a> <a href="../cart.php" id="cart">
+                    <li>Cart</li>
+                </a>
+            </ul>
+        </nav>
+    </header>
+    <!-- End header -->
+    <!-- Feature product -->
+
+    <?php
+    echo '<section id="products">';
+    echo '<h2 class="title">Featured Product</h2>';
+    echo '<div class="container">';
+    echo '<div class="product-container">';
+    echo '<div class="product-card">';
+    echo '<div class="product-name">';
+    echo 'a';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '</section>';
+    ?>
+
+    <!-- New Product -->
+
+    <?php
+    echo '<section id="products">';
+    echo '<h2 class="title">New Product</h2>';
+    echo '<div class="container">';
+    echo '<div class="product-container">';
+    echo '<div class="product-card">';
+    echo '<div class="product-name">';
+    echo 'a';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '</section>';
+    ?>
+
+    <!-- Footer -->
+    <footer class="page-footer">
+        <div class="container">
+            <div class="grid-container">
+                <!-- Footer Logo -->
+                <div class="grid-item"> <a href="../../index.php"><img src="https://i.imgur.com/mE6aWmB.png" alt="logo" class="logo-img" /></a> </div>
+                <!-- Quick Link -->
+                <div class="grid-item inner-grid-container">
+                    <div class="grid-item"> <a href="../about.php">About Us</a> </div>
+                    <div class="grid-item"> <a href="../fees.html">Fees</a> </div>
+                    <div class="grid-item"> <a href="../browse-menu.html">Browse</a> </div>
+                    <div class="grid-item"> <a href="../term_of_services.php">Term of Service</a> </div>
+                    <div class="grid-item"> <a href="../account/account.php">Account</a> </div>
+                    <div class="grid-item"><a href="../faq.html">FAQs</a></div>
+                    <div class="grid-item"> <a href="../contact.html">Contact</a> </div>
+                    <div class="grid-item"> <a href="../privacy_policies.php">Privacy Policy</a> </div>
+                </div>
+                <!-- Social Link -->
+                <div class="grid-item">
+                    <div class="social-buttons"> <a href=""><i class="fab fa-instagram circle-icon"></i></a> <a href=""><i class="fab fa-facebook circle-icon"></i></a> <a href=""><i class="fab fa-linkedin-in circle-icon"></i></a> <a href=""><i class="fab fa-twitter circle-icon"></i></a> </div>
+                </div>
+            </div>
+            <hr />
+            <!-- Copyright -->
+            <p>&copy 2021 | RMIT University | Group 16</p>
+        </div>
+    </footer>
 
     <script type="text/javascript" src="../js/index.js"></script>
 
