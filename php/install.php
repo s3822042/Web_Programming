1b512c2037a93e053d1cc6da29f7537eab5d2975
s3822042 <s3822042@rmit.edu.vn>
2021-05-25 10:12:20 +0700

    fix password retype bug

--- php/install.php
+++ php/install.php
@@ -1,34 +1,34 @@
 <?php
-    // check session status and start session 
-	if ( empty(session_id()) ) session_start();
+// check session status and start session 
+if (empty(session_id())) session_start();
 
-    function encrypt_decrypt($string, $action = 'encrypt')
-    {
-        $encrypt_method = "AES-256-CBC";
-        $secret_key = 'AA74CDCC2BBRT935136HH7B63C27'; // user define private key
-        $secret_iv = '5fgf5HJ5g27'; // user define secret key
-        $key = hash('sha256', $secret_key);
-        $iv = substr(hash('sha256', $secret_iv), 0, 16); // sha256 is hash_hmac_algo
-        if ($action == 'encrypt') {
-            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-            $output = base64_encode($output);
-        } else if ($action == 'decrypt') {
-            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
-        }
-        return $output;
+function encrypt_decrypt($string, $action = 'encrypt')
+{
+    $encrypt_method = "AES-256-CBC";
+    $secret_key = 'AA74CDCC2BBRT935136HH7B63C27'; // user define private key
+    $secret_iv = '5fgf5HJ5g27'; // user define secret key
+    $key = hash('sha256', $secret_key);
+    $iv = substr(hash('sha256', $secret_iv), 0, 16); // sha256 is hash_hmac_algo
+    if ($action == 'encrypt') {
+        $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+        $output = base64_encode($output);
+    } else if ($action == 'decrypt') {
+        $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
     }
+    return $output;
+}
 
-    if (isset($_POST['username']) && isset($_POST['password_retype'])) {
-        $username = $_POST['username'];
-        $pwd = $_POST['password'];
-        $pwd = encrypt_decrypt($pwd, 'encrypt');
-        $fp = fopen('data.txt', 'w');
-        fwrite($fp, $username);
-        fwrite($fp, "\n");
-        fwrite($fp, $pwd);
-        fclose($fp);
-        exit("Admin account created successfully! You can now close this tab!");
-    }
+if (isset($_POST['username']) && isset($_POST['password_retype'])) {
+    $username = $_POST['username'];
+    $pwd = $_POST['password'];
+    $pwd = encrypt_decrypt($pwd, 'encrypt');
+    $fp = fopen('../../data.txt', 'w');
+    fwrite($fp, $username);
+    fwrite($fp, "\n");
+    fwrite($fp, $pwd);
+    fclose($fp);
+    exit("Admin account created successfully! You can now close this tab!");
+}
 ?>
 
 <!DOCTYPE html>
@@ -106,22 +106,26 @@
         </div>
         <div class="form-control">
             <label for="password">Password</label>
-            <input id="password" type="password" name="password" />
+            <input id="password" type="password" name="password" onChange="onChange()" />
         </div>
         <div class="form-control">
             <label for="password_retype">Retype Password</label>
-            <input id="password_retype" type="password" name="password_retype" />
+            <input id="password_retype" type="password" name="password_retype" onChange="onChange()" />
         </div>
         <div class="form-control">
-            <button class="button" type="submit" onclick="displayTest()">Register</button>
+            <input class="button" type="submit" value="Register" />
         </div>
     </form>
 </body>
 <script>
-    function displayTest() {
-        var pwd = document.getElementById("password").value;
-        var pwd_retype = document.getElementById("password_retype").value;
-        if (pwd != pwd_retype) alert("Password do not match");
+    function onChange() {
+        var pwd = document.querySelector('input[name=password]');
+        var pwd_retype = document.querySelector('input[name=password_retype]')
+        if (pwd.value === pwd_retype.value) {
+            pwd_retype.setCustomValidity('');
+        } else {
+            pwd_retype.setCustomValidity('Passwords do not match');
+        }
     }
 </script>
 
